72e7f08d0a797feddd7f264c82ec1ae1725db4e3
Yann Malet <yann.malet@gmail.com>
2012-01-14 14:16:33 +0100

    Improve the setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import os
 from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 VERSION = __import__('kip').__version__
 
@@ -8,6 +16,7 @@ setup(
     author='Graham King',
     author_email='',
     description="kip Keeps Internet Passwords",
+    long_description=read('README.md'),
     packages=find_packages(),
     package_data={},
     scripts = ['kip.py'],
